4cd9138985adaadf261741136abbe9e9d87dc879
evgscorp <evgfree@gmail.com>
2018-12-18 18:44:18 +0300

    builder upd

---
+++ CreationalPatterns/Builder/Tournament.mjs
@@ -0,0 +1,11 @@
+export class Tournament {
+    /**
+     *Creates an instance of Tournament.
+     * @param {string} $name
+     * @memberof Tournament
+     */
+    constructor($name){
+        this.name=name;
+        this.Events = [];
+    }
+} 
