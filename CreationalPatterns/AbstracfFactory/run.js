cf0ffa93666409d67665547b8d522fdc2ebfff52
evgscorp <evgfree@gmail.com>
2018-12-18 08:34:48 +0300

    ambstract factory upd

--- CreationalPatterns/AbstracfFactory/run.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-import {LannisterFactory} from 'LannisterFactory.js';
-import {CourtSession} from 'CourtSession.js';
-
-let courtSession1 = new CourtSession(new LannisterFactory());
-courtSession1.complaintPresented(8);
-courtSession1.complaintPresented(10);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My patterns implementation with ES6+ and Node.js
+# Plese use "node --experimental-modules run.mjs" to run an example
