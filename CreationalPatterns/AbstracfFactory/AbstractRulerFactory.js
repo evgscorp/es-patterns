9156e3abc4f47583023b5e214e44f2a618e2641a
evgscorp <evgfree@gmail.com>
2018-12-18 07:54:55 +0300

    ambstract factory upd

--- CreationalPatterns/AbstracfFactory/AbstractRulerFactory.js
+++ CreationalPatterns/AbstracfFactory/AbstractRulerFactory.js
@@ -1,10 +1,18 @@
 import {AbstractRuler} from 'AbstractRuler.js';
 export class AbstractRulerFactory {
 
+    /**
+     * @returns {AbstractRuler}
+     * @memberof AbstractRulerFactory
+     */
     getKing() {
       return new AbstractRuler(5,10,'the king');  
     }
 
+    /**
+     * @returns {AbstractRuler}
+     * @memberof AbstractRulerFactory
+     */
     getHandOfTheKing() {
         return new  AbstractRuler(10,5,'the hand of king');  
     }
--- CreationalPatterns/AbstracfFactory/LannisterFactory.js
+++ CreationalPatterns/AbstracfFactory/LannisterFactory.js
@@ -3,15 +3,22 @@ import {KingJoffery} from 'KingJoffery.js';
 import {LordTywin} from './LordTywin.js';
 
 
-class LannisterFactory extends AbstractRulerFactory{
+export class LannisterFactory extends AbstractRulerFactory{
     constructor(){
         super();
     }
-    
+    /**
+     * @returns {AbstractRuler}
+     * @memberof LannisterFactory
+     */
     getKing() {
         return new KingJoffery();  
       }
-  
+      
+     /**
+     * @returns {AbstractRuler}
+     * @memberof LannisterFactory
+     */ 
      getHandOfTheKing() {
           return new  LordTywin();  
      } 
