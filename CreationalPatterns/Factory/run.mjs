b6332ee62ed9ac41b16ab3122fe8985be38e16f3
evgscorp <evgfree@gmail.com>
2018-12-20 02:39:49 +0300

    factory

--- CreationalPatterns/Factory/run.mjs
+++ CreationalPatterns/Factory/run.mjs
@@ -1,6 +1,6 @@
 'use strict';
 import {Factory} from './Factory.mjs';
-let FactoryInst = new Factory()
+let FactoryInst = new Factory();
 
 console.log(FactoryInst.getHero('magic','wizard 1'));
 console.log(FactoryInst.getHero('magic','wizard 2'));
