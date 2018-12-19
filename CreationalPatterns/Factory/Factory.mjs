a55b289119bed539c4d80e6543220723fa1d6e26
evgscorp <evgfree@gmail.com>
2018-12-20 02:39:28 +0300

    factory

--- CreationalPatterns/Factory/Factory.mjs
+++ CreationalPatterns/Factory/Factory.mjs
@@ -2,14 +2,19 @@ import {WizardHero} from './WizardHero.mjs';
 import {Barbarian} from './Barbarian.mjs';
 
 export class Factory {
+ 
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
     getHero(type,name){
         switch (type) {
             case 'magic':
-                return new WizardHero(1+this.getRandomInt(3), 10+this.getRandomInt(5), 3+getRandomInt(3), name);
+                return new WizardHero(1+this.getRandomInt(3), 10+this.getRandomInt(5), 3+this.getRandomInt(3), name);
                 break;
 
                 case 'strenght':
-                return new Barbarian(10+this.getRandomInt(5), 1+this.getRandomInt(3), 7+getRandomInt(7), name);
+                return new Barbarian(10+this.getRandomInt(5), 1+this.getRandomInt(3), 7+this.getRandomInt(7), name);
                 break;
         
             default:
@@ -17,7 +22,4 @@ export class Factory {
         }
     }
 
-   getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-      }
 }
