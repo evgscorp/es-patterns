a48f7db01c41254d2378db85377f3179c4d5cb3e
evgscorp <evgfree@gmail.com>
2018-12-21 02:45:15 +0300

    singleton

--- CreationalPatterns/Singleton/run.mjs
+++ CreationalPatterns/Singleton/run.mjs
@@ -1,5 +1,6 @@
 'use strict';
 import {Westeros} from './Westeros.mjs';
+import {Wall} from './Wall.mjs';
 let WesterosInst = new Westeros();
 WesterosInst.Wall.getStatus();
 WesterosInst.Wall.setHeight(250);
@@ -8,4 +9,5 @@ WesterosInst.Wall.getStatus();
 let WesterosInst2 = new Westeros();
 WesterosInst2.Wall.getStatus();
 WesterosInst.Wall.getStatus();
+Wall._instance.getStatus();
 
